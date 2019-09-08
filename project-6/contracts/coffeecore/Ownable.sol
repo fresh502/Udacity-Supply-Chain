4df79696405a6bff58151efecd2e57d314626d93
Sinrok Kim <fresh502@gmail.com>
2019-09-08 15:24:08 +0900

    feat: update role, supplychain, ownable contract

--- project-6/contracts/coffeecore/Ownable.sol
+++ project-6/contracts/coffeecore/Ownable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.4.22 <0.6.0;
 
 /// Provides basic authorization control
 contract Ownable {
@@ -20,7 +20,7 @@ contract Ownable {
 
     /// Define a function modifier 'onlyOwner'
     modifier onlyOwner() {
-        require(isOwner());
+        require(isOwner(), "check owner");
         _;
     }
 
@@ -42,7 +42,7 @@ contract Ownable {
 
     /// Define an internal function to transfer ownership
     function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0));
+        require(newOwner != address(0), "check valid address");
         emit TransferOwnership(origOwner, newOwner);
         origOwner = newOwner;
     }
